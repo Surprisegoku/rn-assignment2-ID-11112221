2efff363b6aa7493d71e7dc0f86f8af1322d5c3a
Surprise <surprisegoku18@gmail.com>
2024-05-24 16:02:20 +0000

    My name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>My name is Goku Surprise</Text>
       <StatusBar style="auto" />
     </View>
   );
